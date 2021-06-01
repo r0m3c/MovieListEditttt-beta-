74505d78becf58a09b0c613e0a305ff650c6403e
r0m3c <leandrogamarra@rocketmail.com>
2021-05-31 21:19:55 -0400

    Initial Commit

---
+++ MovieListEditttt/MovieListEdittttApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListEdittttApp.swift
+//  MovieListEditttt
+//
+//  Created by Leandro Gamarra on 5/31/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieListEdittttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
