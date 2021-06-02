e67229ce1a529a8f1e2bc0190212fbc2ef2d30e6
r0m3c <leandrogamarra@rocketmail.com>
2021-06-02 16:47:17 -0400

    d

--- MovieListEditttt/ContentView.swift
+++ MovieListEditttt/ContentView.swift
@@ -84,6 +84,7 @@ struct movieRow: View {
     var body: some View {
             VStack {
                 
+                //d
                 Image(movieAdd.movieImage)
                     .resizable()
                     .frame(width: 100, height: 100)
